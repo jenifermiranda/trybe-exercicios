5a7ad454244d7464c594398a4d820b0b53d26a9c
Jenifer <jnf.asm@gmail.com>
2023-02-28 18:44:52 -0300

    exercicio 2

--- Fundamentos/secao3-introducao-javascript/dia2-javascript-array-loop-for/script.js
+++ Fundamentos/secao3-introducao-javascript/dia2-javascript-array-loop-for/script.js
@@ -13,6 +13,12 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
-}
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+
+let sum = 0
+for (let index = 0; index < numbers.length; index +=1 ) {
+    sum += numbers[index];
+    
+} console.log(sum);
