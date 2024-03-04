aeca625d49a89fdea6af8601a6fa6bea8e3bb330
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-03-04 13:17:33 +0000

    update

---
+++ Python/secao05/dia02/events/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from events.views import event_details, index, about
+
+urlpatterns = [
+    path("", index, name="home-page"),
+    path("about", about, name="about-page"),
+    path("events/<int:event_id>", event_details, name="details-page"),
+    #   path('/rota', funcao-q-sera-executada, name='nome-q-identifica-a-rota)
+]
